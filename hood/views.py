3a9f541680a9290831c879eca50a7a849f18ca91
Jefferson Gakuya <jeffersongakuya@gmail.com>
2021-04-12 11:14:01 +0300

    Add sidebar and navbar

--- hood/views.py
+++ hood/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required(login_url='accounts/login/')
 def index(request):
+    if request.method == "POST":
+        form = P
     return render(request, "main/index.html")
